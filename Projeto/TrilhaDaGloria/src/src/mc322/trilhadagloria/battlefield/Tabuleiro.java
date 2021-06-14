722b69ba3ff5d439bd59a481517c705eab2e8193
João Matoso <jvematoso@gmail.com>
2021-06-14 16:11:12 -0300

    Gerador de terrenos

--- Projeto/TrilhaDaGloria/src/src/mc322/trilhadagloria/battlefield/Tabuleiro.java
+++ Projeto/TrilhaDaGloria/src/src/mc322/trilhadagloria/battlefield/Tabuleiro.java
@@ -1,10 +1,15 @@
 package mc322.trilhadagloria.battlefield;
 
+import mc322.trilhadagloria.controle.Gerador;
+
 public class Tabuleiro {
 	private Terreno mapa[][];
 	
 	public Tabuleiro() {
 		mapa = new Terreno[5][5];
 		
+		for(int i = 0; i < 5; i++)
+			for(int j = 0; j < 5; j++)
+				mapa[i][j]= Gerador.gerarTerreno();
 	}
 }
--- Projeto/TrilhaDaGloria/src/src/mc322/trilhadagloria/controle/Gerador.java
+++ Projeto/TrilhaDaGloria/src/src/mc322/trilhadagloria/controle/Gerador.java
@@ -2,6 +2,15 @@ package mc322.trilhadagloria.controle;
 
 import java.util.Random;
 
+import mc322.trilhadagloria.battlefield.Caverna;
+import mc322.trilhadagloria.battlefield.Deserto;
+import mc322.trilhadagloria.battlefield.Floresta;
+import mc322.trilhadagloria.battlefield.Montanha;
+import mc322.trilhadagloria.battlefield.Oceano;
+import mc322.trilhadagloria.battlefield.Planicie;
+import mc322.trilhadagloria.battlefield.Terreno;
+import mc322.trilhadagloria.battlefield.Tundra;
+import mc322.trilhadagloria.battlefield.Vulcanico;
 import mc322.trilhadagloria.monarch.Armadilha;
 import mc322.trilhadagloria.monarch.Barbaro;
 import mc322.trilhadagloria.monarch.Bardo;
@@ -58,6 +67,28 @@ public class Gerador {
 	
 	public static Carta gerarArmadilha() {
 		return new Armadilha();
+	}
+	
+	public static Terreno gerarTerreno() {
+		Random rnd = new Random();
+		int p = rnd.nextInt(100);
+		
+		if(p < 13)
+			return new Deserto();
+		else if(p >= 13 && p < 25)
+			return new Floresta();
+		else if(p >= 25 && p < 37)
+			return new Tundra();
+		else if(p >= 37 && p < 50)
+			return new Oceano();
+		else if(p >= 50 && p < 63)
+			return new Vulcanico();
+		else if(p >= 63 && p < 76)
+			return new Montanha();
+		else if(p >= 76 && p < 86)
+			return new Planicie();
+		else
+			return new Caverna();
 	}
 	
 }
