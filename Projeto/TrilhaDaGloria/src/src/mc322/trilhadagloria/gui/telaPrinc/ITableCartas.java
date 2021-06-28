881459709129db8b958e31695093c9c246b005df
João Matoso <jvematoso@gmail.com>
2021-06-28 06:26:17 -0300

    interface tabela

---
+++ Projeto/TrilhaDaGloria/src/src/mc322/trilhadagloria/gui/telaPrinc/ITableCartas.java
@@ -0,0 +1,9 @@
+package mc322.trilhadagloria.gui.telaPrinc;
+
+import mc322.trilhadagloria.carta.IStatusCarta;
+
+public interface ITableCartas {
+	public void adicionarCarta(IStatusCarta carta);
+	
+	public void removerCarta(IStatusCarta carta);
+}
